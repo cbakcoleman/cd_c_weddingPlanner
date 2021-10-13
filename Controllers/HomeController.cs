1106ac5bd30523a11191ab8ef056d60cd9a6f455
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-10-12 19:55:35 -0700

    begin db connect

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using cd_c_weddingPlanner.Models;
+
+namespace cd_c_weddingPlanner.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyContext _context;
+        public HomeController(MyContext context)
+        {
+            _context = context;
+        }
+    }
+}
